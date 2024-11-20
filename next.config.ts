edf4c742898c922376c551b8b4b322cae314da4c
Eda Nur Nair <93126814+EdANaiR@users.noreply.github.com>
2024-11-20 20:45:48 +0300

    Second commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverActions: {
       allowedOrigins: ["*"],
     },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
